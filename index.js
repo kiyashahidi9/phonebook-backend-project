9858def6291565242c627b37acd4f9ac2c8cf7ba
Kiya Shahidi <kiyashahidi9@gmail.com>
2026-07-16 12:34:09 -0400

    eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // IMPORTS
-require('dotenv').config();
-const express = require('express');
-const Person = require('./models/person');
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
 // MIDDLEWARE
-app.use(express.json());
-app.use(express.static('dist'));
+app.use(express.json())
+app.use(express.static('dist'))
 
 // INFO
 app.get('/info', (request, response) => {
@@ -21,79 +21,79 @@ app.get('/info', (request, response) => {
 // GET PERSON(S) ROUTES
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
-    response.json(persons);
+    response.json(persons)
   })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  let id = request.params.id;
+  let id = request.params.id
 
   Person.findById(id)
     .then(person => {
       if (!person) response.status(404).end()
-      response.json(person);
+      response.json(person)
     })
-    .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 // DELETE PERSON ROUTES
 app.delete('/api/persons/:id', (request, response, next) => {
-  let id = request.params.id;
+  let id = request.params.id
 
-  Person.findByIdAndDelete(id).then(result => {
-    response.status(204).end();
+  Person.findByIdAndDelete(id).then(() => {
+    response.status(204).end()
   })
-  .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 // POST PERSON ROUTES
 app.post('/api/persons', (request, response, next) => {
-  let body = request.body;
+  let body = request.body
 
   let person = new Person({
     name: body.name,
     number: body.number,
   })
   person.save().then(person => {
-    response.json(person);
+    response.json(person)
   })
-  .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 // UPDATE NUMBER
 app.patch('/api/persons/:id', (request, response, next) => {
-  let body = request.body;
-  let id = request.params.id;
+  let body = request.body
+  let id = request.params.id
 
   Person.findById(id).then(person => {
-    if (!person) response.status(404).end();
+    if (!person) response.status(404).end()
 
-    person.number = body.number;
+    person.number = body.number
 
     person.save().then(updatedPerson => {
       response.json(updatedPerson)
     })
   })
-  .catch(error => next(error));
+    .catch(error => next(error))
 })
 
 // ERROR MIDDLEWARE
 function errorHandler(error, request, response, next) {
-  console.log(error);
+  console.log(error)
 
   if (error.name === 'CastError') {
-    response.status(400).send({error: "malformatted id"})
+    response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    response.status(400).send({error: error.message});
+    response.status(400).send({ error: error.message })
   }
 
-  next(error);
+  next(error)
 }
 
 app.use(errorHandler)
 
 // STARTING APP
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,17 +1,17 @@
 // DNS CONFIG
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 
 // MONGOOSE CONFIG AND DB CONNECTION
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
-mongoose.connect(url, {family: 4})
-  .then(result => {
-    console.log('Connected to MongoDB');
+const url = process.env.MONGODB_URI
+mongoose.connect(url, { family: 4 })
+  .then(() => {
+    console.log('Connected to MongoDB')
   })
   .catch(error => {
-    console.log('Failed connnection to MongoDB:', error.message);
+    console.log('Failed connnection to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
@@ -25,7 +25,7 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator(v) {
-        return /^\d{2,3}-\d+$/.test(v);
+        return /^\d{2,3}-\d+$/.test(v)
       }
     },
     required: true,
@@ -34,12 +34,12 @@ const personSchema = new mongoose.Schema({
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
 })
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
-module.exports = Person;
+module.exports = Person
